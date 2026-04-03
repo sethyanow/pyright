2b3c56bdb47c68aca44ae353ae1b164318e3523f
Seth <sethyanow@users.noreply.github.com>
2026-04-03 14:25:03 -0400

    Revert "refactor(analyzer): extract matchArgsToParams to callValidation.ts"
    This reverts commit 59697c0de695f63600488e98c92f281bf236c432.

--- packages/pyright-internal/src/analyzer/typeEvaluator.ts
+++ packages/pyright-internal/src/analyzer/typeEvaluator.ts
@@ -8219,15 +8219,6 @@ export function createTypeEvaluator(
         typeResult: TypeResult<FunctionType>,
         overloadIndex: number
     ): MatchArgsToParamsResult {
-        return callValidation.matchArgsToParams(evaluatorInterface, state, registry, errorNode, argList, typeResult, overloadIndex);
-    }
-
-    function _matchArgsToParams_dead(
-        errorNode: ExpressionNode,
-        argList: Arg[],
-        typeResult: TypeResult<FunctionType>,
-        overloadIndex: number
-    ): MatchArgsToParamsResult {
         const overload = typeResult.type;
         const paramDetails = getParamListDetails(overload, { disallowExtraKwargsForTd: true });
         const paramSpec = FunctionType.getParamSpecFromArgsKwargs(overload);
